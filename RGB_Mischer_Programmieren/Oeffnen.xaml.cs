2c5a96b47f684f7a471cb10eda08c47c1ea8a5d3
Niklas Schmeiser <schmeiserniklas@gmail.com>
2022-04-15 15:26:48 +0200

    Form für das auslesen von Color-Objekten bereitgestellt.

---
+++ RGB_Mischer_Programmieren/Oeffnen.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RGB_Mischer_Programmieren
+{
+    /// <summary>
+    /// Interaktionslogik für Oeffnen.xaml
+    /// </summary>
+    public partial class Oeffnen : Window
+    {
+        public Oeffnen()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_WeiterClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_AbbrechenClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
